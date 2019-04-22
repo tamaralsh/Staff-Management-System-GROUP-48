395aa83c1fa820405b614bc47ba746983d792f89
zihaolam <u3554914@connect.hku.hk>
2019-04-23 02:56:52 +0800

    Update functions.h

--- functions.h
+++ functions.h
@@ -32,13 +32,14 @@ public:
     string employeeID;
     string password;
   };
-    
+  
   static bool IDpass_check(int i, vector<IDpass> UserIDpass, string password);
   static int employeeID_search(string employeeID, vector<IDpass> UserIDpass);
   static void search_bySalary(double salary, vector<Details> Employee_details,string high_or_low);
   static int search_byName(string name, vector<Details> Employee_details);
   static int search_byEmployeeID(string employee_ID, vector<Details> Employee_details);
   static void print_history(vector<string> history);
+  static string password_randomizer();
   static void print_history_delete(vector<string> history);
   static string login_page(vector<IDpass> UserIDpass);
   static void print_details(Details Employee_details);
@@ -48,6 +49,7 @@ public:
   static void createEmployee(vector<Details> &Employee_details);
   static void save_details(string employeeData_filename, vector<Details> Employee_details);
   static void get_Positions(vector<Details> Employee_details,vector<string> &Positions);
+  static string store_UserIDpass(string UserIDpass_filename, string employeeID);
 };
 
 #endif // CREATENEDIT_H
