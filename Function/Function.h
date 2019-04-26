d6cd1d4909b582403c973f0defb0716626908305
zihaolam <u3554914@connect.hku.hk>
2019-04-27 02:42:40 +0800

    Update Function.h

--- Function/Function.h
+++ Function/Function.h
@@ -67,6 +67,7 @@ public:
   static int delete_byName(vector<Details> &Employee_details, string name);
   static void addUserAttribute(vector<Details> & Employee_details, string attribute);
   static void add_attribute_toAll(vector<Details> & Employee_details, vector<string> buffer_attribute);
+  static void search_byAge(string yeaRofBirth,vector<Details> Employee_details);
 
 
 };
