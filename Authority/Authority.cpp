dbae2754074f4d643deae70e70ae822aaf8b4489
tamaralsh <47145602+tamaralsh@users.noreply.github.com>
2019-04-27 17:26:39 +0800

    Update Authority.cpp

--- Authority/Authority.cpp
+++ Authority/Authority.cpp
@@ -38,25 +38,25 @@ int Authority::menu_display_authorized()
 {
   int selection;
   cout << endl;
-  cout << "--------- High Authority Staff Management System ---------" << endl;
-  cout << "1. Search by name" << endl;
-  cout << "2. Search by employee ID" << endl;
-  cout << "3. Search by age" << endl;
-  cout << "4. Search by salary" << endl;
-  cout << "5. Search by job position" << endl;
-  cout << "6. Show KPI leaderboard" << endl;
-  cout << "7. Create New Employee" << endl;
-  cout << "8. Delete employee details" << endl;
-  cout << "9. Edit employee details" << endl;
-  cout << "0. Exit" << endl;
+  cout << "--------------- High Authority Staff Management System ---------------" << endl;
+  cout << "1. Search by name." << endl;
+  cout << "2. Search by employee ID." << endl;
+  cout << "3. Search by age." << endl;
+  cout << "4. Search by salary." << endl;
+  cout << "5. Search by job position." << endl;
+  cout << "6. Show KPI leaderboard." << endl;
+  cout << "7. Create New Employee." << endl;
+  cout << "8. Delete employee details." << endl;
+  cout << "9. Edit employee details." << endl;
+  cout << "0. Exit." << endl;
   
   cout << endl << "Enter your choice: ";
   cin >> selection;
   cout << endl;
   while (selection < 0 || selection > 9)
   {
-    cout << "Please choose an option from the menu" << endl;
-    cout << "Enter option : ";
+    cout << "Please choose an option from the menu." << endl;
+    cout << "Enter option: ";
     cin >> selection;
     cout << endl;
   }
